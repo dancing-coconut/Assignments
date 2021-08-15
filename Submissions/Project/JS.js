650f68365707237868692195113d9496b87fec7e
Dancing Coconut <dancingcoconut12@gmail.com>
2021-08-15 18:22:11 +0530

    Change-6

--- Submissions/Project/JS.js
+++ Submissions/Project/JS.js
@@ -1,18 +1,18 @@
 document.onmousemove = animateCircles; 
 var colors = ['#dfecfc','#bfdaf7','#7bb3f0','#378ce7','#1667b9']
 function animateCircles(event) {
-  var circle = document.createElement("div");
-  circle.setAttribute("class", "circle");
-  document.body.appendChild(circle); 
-  circle.style.left = event.clientX + 'px';
-  circle.style.top = event.clientY + 'px';
+  var square = document.createElement("div");
+  square.setAttribute("class", "square");
+  document.body.appendChild(square); 
+  square.style.left = event.clientX + 'px';
+  square.style.top = event.clientY + 'px';
   var color = colors[Math.floor(Math.random() * colors.length)];
-  circle.style.borderColor = color;
-  circle.style.transition = "all 0.5s linear 0s";
-  circle.style.left = circle.offsetLeft - 20 + 'px';
-  circle.style.top = circle.offsetTop - 20 + 'px';
-  circle.style.width = "50px";
-  circle.style.height = "50px";
-  circle.style.borderWidth = "5px";
-  circle.style.opacity = 0;
+  square.style.borderColor = color;
+  square.style.transition = "all 0.5s linear 0s";
+  square.style.left = square.offsetLeft - 20 + 'px';
+  square.style.top = square.offsetTop - 20 + 'px';
+  square.style.width = "50px";
+  square.style.height = "50px";
+  square.style.borderWidth = "5px";
+  square.style.opacity = 0;
 }
