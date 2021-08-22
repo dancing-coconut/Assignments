2ccf9814b163dbd234faaf8820a18b1e26f967c9
Dancing Coconut <dancingcoconut12@gmail.com>
2021-08-22 14:28:30 +0530

    Module 5

--- Submissions/Project/HelpNest/Contact.php
+++ Submissions/Project/HelpNest/Contact.php
@@ -1,11 +1,14 @@
-<?php
-if (isset($_POST['submit'])) {
-    $name=$_POST['name'];
-    $mailFrom=$_POST['mail'];
-    $subject=$_POST['subject'];
-    $message=$_POST['message'];
-    $mailTo=saipriya.rajagopal2020@vitstudent.ac.in;
-    $headers="From: ".$mail;
-    mail($mailTo,$subject,$headers);
-    header("Location: index.php?mailsend");
-}
+<?php 
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name'];
+        $mailFrom = $_POST['mail'];
+        $subject = $_POST['subject'];
+        $Msg = $_POST['message'];
+        $to = "saipriya.rajagopal2020@vitstudent.ac.in";
+        if(mail($mailTo,$subject,$message,$mailFrom))
+        {
+               header("location:index.php?success");
+        }
+    }
+?>
